c8838e95163e9e92b463ba7419f615f72bb72510
Andrey Bubyr <abubyr@mirantis.com>
2014-03-07 14:17:40 +0200

    mogno manifests filenames corrected in apply.sh

--- apply.sh
+++ apply.sh
@@ -31,9 +31,9 @@ cp -af astute.yaml /etc/
 cd ..
 
 if [ PRIMARY_MONGO -eq 1 ]; then
-    puppet apply --logdest /root/primary_mongo.pp.log --verbose --debug --trace "${PUPPET_MANIFESTS}/primary_mongo.pp"
+    puppet apply --logdest /root/primary-mongo.pp.log --verbose --debug --trace "${PUPPET_MANIFESTS}/primary-mongo.pp"
     sleep 120
     puppet apply --logdest /root/ceilometer.pp.log --verbose --debug --trace "${PUPPET_MANIFESTS}/ceilometer.pp"
 else
-    puppet apply --logdest /root/secondary_mongo.pp.log --verbose --debug --trace "${PUPPET_MANIFESTS}/secondary_mongo.pp"
+    puppet apply --logdest /root/secondary-mongo.pp.log --verbose --debug --trace "${PUPPET_MANIFESTS}/secondary-mongo.pp"
 fi
